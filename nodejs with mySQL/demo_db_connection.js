49ad1456e1befaa6fa491053822b01c5de025121
charankrishna <charan.nithinj@gmail.com>
2018-12-01 14:20:30 +0530

    Sql Connection esstablished

---
+++ nodejs with mySQL/demo_db_connection.js
@@ -0,0 +1,15 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+	host:"localhost",
+	user:"root",
+	password:""
+});
+
+con.connect(function(err){
+	if (err) throw err;
+	 console.log("Connected");
+	 con.query(sql, function(err,result){
+	 	console.log("Result:"+result);
+	 });
+});
